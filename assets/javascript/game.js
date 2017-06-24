2dc4bbab531943cbc1f4cf739b0e04c4d4f0a3eb
Ryan Bregier <rabunc@gmail.com>
2017-06-23 22:11:24 -0400

    fixed errors

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
 
     var characterList = {
         LukeSkywalker: {
-            name: "Luke Skywalker"
-            nickname: "Luke"
+            name: "Luke Skywalker",
+            nickname: "Luke",
             image: "assets/images/luke-skywalker.jpg",
             health: 100,
             attackpower: 25,
         },
         ObiWanKenobi: {
-            name: "ObiWan Kenobi"
-            nickname: "Obi"
+            name: "ObiWan Kenobi",
+            nickname: "Obi",
             image: "assets/images/Ben_Kenobi.png",
             health: 120,
             attackpower: 20,
@@ -22,36 +22,37 @@ $(document).ready(function () {
 
     // game status checks
     var characterSelected = false;
-
-    for (var i in characterList) {
-        var characterOptions = $("li");
-        $(characterOptions).html("<img src=" + characterList[i].image + ">");
-        $(characterOptions).attr("id", characterList[i].nickname);
-        $(characterOptions).addClass("character-box img img-responsive img-thumbnail");
-        $("#staging-area").append(characterOptions);
+    
+    function generateCharacterList() {
+        for (var i in characterList) {
+            var characterOptions = $("li");
+            $(characterOptions).html("<img src=" + characterList[i].image + ">");
+            $(characterOptions).attr("id", characterList[i].nickname);
+            $(characterOptions).addClass("character-box img img-responsive img-thumbnail");
+            $("#staging-area").append(characterOptions);
+        }
     }
+        // function generateCharacterList() {
+        //     for (i = 0; i < characterArray.length; i++) {
+        //         var characterOptions = $("<li>");
+        //         $(characterOptions).html("<img src=" + characterImages[i] + ">");
+        //         $(characterOptions).attr("id", characterArray[i]);
+        //         $(characterOptions).addClass("character-box img img-responsive img-thumbnail");
+        //         //$(characterOptions).text(characterArray[i]);
+        //         console.log(characterOptions);
+        //         $("#staging-area").append(characterOptions);
+        //     };
+        // }
 
-    // function generateCharacterList() {
-    //     for (i = 0; i < characterArray.length; i++) {
-    //         var characterOptions = $("<li>");
-    //         $(characterOptions).html("<img src=" + characterImages[i] + ">");
-    //         $(characterOptions).attr("id", characterArray[i]);
-    //         $(characterOptions).addClass("character-box img img-responsive img-thumbnail");
-    //         //$(characterOptions).text(characterArray[i]);
-    //         console.log(characterOptions);
-    //         $("#staging-area").append(characterOptions);
-    //     };
-    // }
-
-//     $(".character-box").on("click", selectCharacter)
-//     if (!characterSelected) {
-//         $("#your-character").append(this);
-//     } else {
-//         return
-//     }
-// }
-
+        //     $(".character-box").on("click", selectCharacter)
+        //     if (!characterSelected) {
+        //         $("#your-character").append(this);
+        //     } else {
+        //         return
+        //     }
+        // }
 
 
-    generateCharacterList();
-});
+
+        generateCharacterList();
+    });
