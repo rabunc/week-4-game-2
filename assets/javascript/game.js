3364122ae6d3b7b71ee29a2d6bcdf0e44a785496
Ryan Bregier <rabunc@gmail.com>
2017-06-23 22:12:21 -0400

    working code for review

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,26 +23,26 @@ $(document).ready(function () {
     // game status checks
     var characterSelected = false;
     
-    function generateCharacterList() {
-        for (var i in characterList) {
-            var characterOptions = $("li");
-            $(characterOptions).html("<img src=" + characterList[i].image + ">");
-            $(characterOptions).attr("id", characterList[i].nickname);
-            $(characterOptions).addClass("character-box img img-responsive img-thumbnail");
-            $("#staging-area").append(characterOptions);
+    // function generateCharacterList() {
+    //     for (var i in characterList) {
+    //         var characterOptions = $("li");
+    //         $(characterOptions).html("<img src=" + characterList[i].image + ">");
+    //         $(characterOptions).attr("id", characterList[i].nickname);
+    //         $(characterOptions).addClass("character-box img img-responsive img-thumbnail");
+    //         $("#staging-area").append(characterOptions);
+    //     }
+    // }
+        function generateCharacterList() {
+            for (i = 0; i < characterArray.length; i++) {
+                var characterOptions = $("<li>");
+                $(characterOptions).html("<img src=" + characterImages[i] + ">");
+                $(characterOptions).attr("id", characterArray[i]);
+                $(characterOptions).addClass("character-box img img-responsive img-thumbnail");
+                //$(characterOptions).text(characterArray[i]);
+                console.log(characterOptions);
+                $("#staging-area").append(characterOptions);
+            };
         }
-    }
-        // function generateCharacterList() {
-        //     for (i = 0; i < characterArray.length; i++) {
-        //         var characterOptions = $("<li>");
-        //         $(characterOptions).html("<img src=" + characterImages[i] + ">");
-        //         $(characterOptions).attr("id", characterArray[i]);
-        //         $(characterOptions).addClass("character-box img img-responsive img-thumbnail");
-        //         //$(characterOptions).text(characterArray[i]);
-        //         console.log(characterOptions);
-        //         $("#staging-area").append(characterOptions);
-        //     };
-        // }
 
         //     $(".character-box").on("click", selectCharacter)
         //     if (!characterSelected) {
